d0404e8b80d6bbb8e0527072f26c2fec49bef730
lberger1993 <berger.luciaeve@gmail.com>
2018-05-14 20:44:39 +0200

    Adds beginnign for cart, needs to modify the view and the formatting of the cart

--- secretsapp/secretsmodules/urls.py
+++ secretsapp/secretsmodules/urls.py
@@ -4,5 +4,6 @@ from secretsmodules import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
+    url(r'^add_to_cart/(?P<secret_id>[0-9]+)$', views.add_to_cart, name='add_to_cart'),
     url(r'^register/',views.RegisterForm.as_view(), name='register')
 ]
